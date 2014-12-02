67c6b237977dbaf5f58678169f73fd275de965f0
Alison <alsunnid@gmail.com>
2014-12-02 12:06:00 +0000

    removed NPC and added a lot of comments to Civilian

--- src/ie/itcarlow/box2ddemo/Civilian.java
+++ src/ie/itcarlow/box2ddemo/Civilian.java
@@ -9,6 +9,55 @@ import org.andengine.opengl.texture.region.ITextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Civilian {
-	//
+	//position
+	private Vector2 m_position;
+	//texture
+	private BitmapTextureAtlas mTexture;
+	private ITextureRegion mTextureRegion;
+	private Sprite mSprite, markSprite;
+	//scale
+	private float m_width, m_height, m_scale;
+	//bools
+	private boolean m_marked, m_shot;
+	//(States) Normal, Alert
 	
+	//Marked()
+	//Shot()
+	//Move()
+	//Update()
+	
+	//public void Load(TextureManager mTextureManager) {
+	//	BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/"); 
+	//	mTexture = new BitmapTextureAtlas(mTextureManager, 32, 40);
+		//mTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mTexture, /* context*/ , "tempNPC.png", 0, 0);
+	//	mTexture.load();
+	//}
+	
+	//public boolean CheckMark() {
+		// if(screentaped and crosshair in sprite area)
+		//		shot = true; 
+		// else
+		//		shot = false;
+	//	return marked;
+	//}
+	
+	//public boolean CheckHit() {
+		// if(screentaped and crosshair in sprite area)
+		//		shot = true;
+		// else
+		//		shot = false;
+		
+	//	return shot;
+	//}
+	
+	//public void Update() {
+	//	Move();
+	//}
+	
+	//public void Draw() {
+	//	mSprite = new Sprite(50, 50, this.mTextureRegion, this.getVertexBufferObjectManager());
+		// if CheckMark() == true ) 
+		// 		draw marker;
+	//}
 }
+
