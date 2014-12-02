90d7ce194e4c8e617403ee526bb0ca9de5df27a2
Alison <alsunnid@gmail.com>
2014-12-02 11:55:17 +0000

    Added Civilian.java to replace NCP.java

---
+++ src/ie/itcarlow/box2ddemo/Civilian.java
@@ -0,0 +1,14 @@
+package ie.itcarlow.box2ddemo;
+
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureManager;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Civilian {
+	//
+	
+}
